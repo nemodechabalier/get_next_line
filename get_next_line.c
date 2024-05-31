af1dca922aee1797171763e86ae6f5da556198be
Némo <nemdechab@gmail.com>
2024-05-31 17:14:23 +0200

    valgind error corrected

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,38 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 13:54:56 by ubuntu            #+#    #+#             */
-/*   Updated: 2024/05/31 15:57:52 by ubuntu           ###   ########.fr       */
+/*   Updated: 2024/05/31 17:11:33 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strncat(char *dest, char *src, int n_byte)
+char	*ft_strncat(char *buffer, char *src, int n_byte)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while (dest[i])
+	while (buffer[i])
 		i++;
 	while (src[j] && j < n_byte)
 	{
-		dest[i + j] = src[j];
+		buffer[i + j] = src[j];
 		j++;
 	}
-	dest[i + j] = '\0';
-	return (dest);
+	buffer[i + j] = '\0';
+	return (buffer);
 }
 
 char	*ft_realoc(char *src, int size)
 {
-	char	*dest;
+	char	*buffer;
 	int		i;
 
 	i = 0;
-	dest = malloc(sizeof(char) * (size + 1));
-	if (!dest)
+	buffer = malloc(sizeof(char) * (size + 1));
+	if (!buffer)
 	{
 		if (src)
 			free(src);
@@ -45,45 +45,51 @@ char	*ft_realoc(char *src, int size)
 	}
 	if (!src)
 	{
-		dest[0] = '\0';
-		return (dest);
+		buffer[0] = '\0';
+		return (buffer);
 	}
 	while (src[i])
 	{
-		dest[i] = src[i];
+		buffer[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	buffer[i] = '\0';
 	free(src);
-	return (dest);
+	return (buffer);
+}
+
+char	*get_next_line_all(int fd)
+{
+	char	str[BUFFER_SIZE];
+	char	*buffer;
+	int		size;
+	int		n_byte;
+	int		tot_size;
+
+	buffer = NULL;
+	tot_size = 0;
+	size = BUFFER_SIZE / 2;
+	n_byte = 1;
+	while (n_byte > 0)
+	{
+		n_byte = read(fd, str, BUFFER_SIZE);
+		str[n_byte] = '\0';
+		tot_size = tot_size + n_byte;
+		if (tot_size > size || !buffer)
+		{
+			size *= 2;
+			buffer = ft_realoc(buffer, size);
+			if (!buffer)
+				return (NULL);
+		}
+		ft_strncat(buffer, str, n_byte);
+	}
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	char	str[BUFFER_SIZE];
-	char	*dest;
-	int		size;
-	int		n_byte;
-	int		tot_size;
-
-	dest = NULL;
-	size = BUFFER_SIZE / 2;
-	n_byte = 1;
-	while (n_byte > 0)
-	{
-		n_byte = read(fd, str, BUFFER_SIZE);
-		tot_size = tot_size + n_byte;
-		if (tot_size > size || !dest)
-		{
-			size *= 2;
-			dest = ft_realoc(dest, size);
-			if (!dest)
-				return (NULL);
-		}
-		if (n_byte > 0)
-			ft_strncat(dest, str, n_byte);
-	}
-	return (dest);
+	return (get_next_line_all(fd));
 }
